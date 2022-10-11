54e806f8db68f3aca5aa8260e040c1b28afafbf8
Jackson Morgan <jacksonmorgan@pop-os.localdomain>
2022-10-10 17:00:15 -0700

    altered ini

--- test/test_unit/test_loop.py
+++ test/test_unit/test_loop.py
@@ -4,11 +4,28 @@ from mcdc.loop import loop_particle, loop_main, loop_source
 import mcdc.type_
 
 
-def setup_test():
-    P = particle()
-    S = surface()
+def loop_setup_test():
+    type_.make_type_surface(1)
+    P = np.zeros(1, dtype=type_.particle)[0]
+    S = np.zeros(1, dtype=type_.surface)[0]
+    
+    x     = 5.0
+    trans = np.array([0.0, 0.0, 0.0])
+
+    S['G']      = 1.0
+    S['linear'] = True
+    S['J']      = np.array([-x, -x])
+    S['t']      = np.array([0.0, INF])
+
+    # Surface on the left
+    P['x'] = 4.0
+    
+    return(S, P)
+    
 
 def test_loop_main():
+    
+    
     print('This worked')
     assert 0 == 0
 
@@ -18,8 +35,3 @@ def test_loop_particle():
     print('This worked')
     assert 0 == 0
     
-
-
-
-# TODO testing structre also should have numba test checking!
-# TODO initalize setup numba with version checking
