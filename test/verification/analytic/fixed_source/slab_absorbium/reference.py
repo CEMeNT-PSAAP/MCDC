2de8c2acb5dc48127010038ed5f8ee69b2d9aa13
ilhamv <variansyah_ilham@gmail.com>
2022-11-17 09:15:15 -0800

    fix bug in test

--- test/verification/analytic/fixed_source/slab_absorbium/reference.py
+++ test/verification/analytic/fixed_source/slab_absorbium/reference.py
@@ -19,48 +19,54 @@ def reference(x):
     tau3 = SigmaT3*(x3-x2)
 
     # Angular flux
-    def psi1_plus(mu,x):
-        return q1/SigmaT1*(1.0 - np.exp(-SigmaT1*x/mu))
-    def psi2_plus(mu,x):
-        return (psi1_plus(mu,x1) - q2/SigmaT2)*np.exp(-SigmaT2*(x-x1)/mu) + q2/SigmaT2
-    def psi3_plus(mu,x):
-        return (psi2_plus(mu,x2) - q3/SigmaT3)*np.exp(-SigmaT3*(x-x2)/mu) + q3/SigmaT3
-    def psi1_minus(mu,x): 
-        return (psi2_minus(mu,x1) - q1/SigmaT1)*np.exp(-SigmaT1*(x1-x)/np.abs(mu)) + q1/SigmaT1
-    def psi2_minus(mu,x):
-        return (psi3_minus(mu,x2) - q2/SigmaT2)*np.exp(-SigmaT2*(x2-x)/np.abs(mu)) + q2/SigmaT2
-    def psi3_minus(mu,x):
-        return q3/SigmaT3*(1.0 - np.exp(-SigmaT3*(x3-x)/np.abs(mu)))
+    def psi1(mu,x):
+        if mu > 0.0:
+            return q1/SigmaT1*(1.0 - np.exp(-SigmaT1*x/mu))
+        elif mu < 0.0:
+            return (psi2(mu,x1) - q1/SigmaT1)*np.exp(-SigmaT1*(x1-x)/np.abs(mu)) + q1/SigmaT1
+    def psi2(mu,x):
+        if mu > 0.0:
+            return (psi1(mu,x1) - q2/SigmaT2)*np.exp(-SigmaT2*(x-x1)/mu) + q2/SigmaT2
+        elif mu < 0.0:
+            return (psi3(mu,x2) - q2/SigmaT2)*np.exp(-SigmaT2*(x2-x)/np.abs(mu)) + q2/SigmaT2
+    def psi3(mu,x):
+        if mu > 0.0:
+            return (psi2(mu,x2) - q3/SigmaT3)*np.exp(-SigmaT3*(x-x2)/mu) + q3/SigmaT3
+        elif mu < 0.0:
+            return q3/SigmaT3*(1.0 - np.exp(-SigmaT3*(x3-x)/np.abs(mu)))
 
     # Flux
     def phi1(x):
-        return (quad(psi1_plus, 0, 1, args=(x))[0] + quad(psi1_minus, -1, 0, args=(x))[0])
+        return quad(psi1, -1, 1, args=(x), points=[0.0])[0]
     def phi2(x):
-        return (quad(psi2_plus, 0, 1, args=(x))[0] + quad(psi2_minus, -1, 0, args=(x))[0])
+        return quad(psi2, -1, 1, args=(x), points=[0.0])[0]
     def phi3(x):
-        return (quad(psi3_plus, 0, 1, args=(x))[0] + quad(psi3_minus, -1, 0, args=(x))[0])
+        return quad(psi3, -1, 1, args=(x), points=[0.0])[0]
 
     # Integrands for current
-    def mu_psi1_plus(mu,x):
-        return mu*psi1_plus(mu,x)
-    def mu_psi2_plus(mu,x):
-        return mu*psi2_plus(mu,x)
-    def mu_psi3_plus(mu,x):
-        return mu*psi3_plus(mu,x)
-    def mu_psi1_minus(mu,x):
-        return mu*psi1_minus(mu,x)
-    def mu_psi2_minus(mu,x):
-        return mu*psi2_minus(mu,x)
-    def mu_psi3_minus(mu,x):
-        return mu*psi3_minus(mu,x)
+    def mu_psi1(mu,x):
+        if mu > 0.0:
+            return mu*psi1(mu,x)
+        elif mu < 0.0:
+            return mu*psi1(mu,x)
+    def mu_psi2(mu,x):
+        if mu > 0.0:
+            return mu*psi2(mu,x)
+        elif mu < 0.0:
+            return mu*psi2(mu,x)
+    def mu_psi3(mu,x):
+        if mu > 0.0:
+            return mu*psi3(mu,x)
+        elif mu < 0.0:
+           return mu*psi3(mu,x)
 
     # Current
     def J1(x):
-        return quad(mu_psi1_plus, 0, 1, args=(x))[0] + quad(mu_psi1_minus, -1, 0, args=(x))[0]
+        return quad(mu_psi1, -1, 1, args=(x), points=[0.0])[0]
     def J2(x):
-        return quad(mu_psi2_plus, 0, 1, args=(x))[0] + quad(mu_psi2_minus, -1, 0, args=(x))[0]
+        return quad(mu_psi2, -1, 1, args=(x), points=[0.0])[0]
     def J3(x):
-        return quad(mu_psi3_plus, 0, 1, args=(x))[0] + quad(mu_psi3_minus, -1, 0, args=(x))[0]
+        return quad(mu_psi3, -1, 1, args=(x), points=[0.0])[0]
 
     phi      = np.zeros(60)
     J        = np.zeros(60)
--- test/verification/analytic/run.py
+++ test/verification/analytic/run.py
@@ -13,11 +13,12 @@ N_max = 7
 for task in os.scandir('./fixed_source'):
     os.chdir(task)
     for N_hist in np.logspace(N_min, N_max, (N_max-N_min)*2+1):
-        print(task, int(N_hist))
-        if N_proc == 1:
-            os.system("python input.py --mode=numba %i"%(N_hist))
-        else:
-            os.system("srun -n %i python input.py --mode=numba %i"%(N_proc,N_hist))
+        if not os.path.isfile('output_'+str(int(N_hist))+'.h5'):
+            print(task, int(N_hist))
+            if N_proc == 1:
+                os.system("python input.py --mode=numba %i"%(N_hist))
+            else:
+                os.system("srun -n %i python input.py --mode=numba %i"%(N_proc,N_hist))
     os.chdir(r"../..")
 
 # Eigenvalue
