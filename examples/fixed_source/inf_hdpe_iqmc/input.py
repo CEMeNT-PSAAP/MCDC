14ab08e6fed742811c4c33b8436d4304b5f11084
Sam Pasmann <spasmann@nd.edu>
2023-02-13 08:51:07 -0700

    iQMC examples black style

--- examples/fixed_source/inf_hdpe_iqmc/input.py
+++ examples/fixed_source/inf_hdpe_iqmc/input.py
@@ -7,7 +7,7 @@ import mcdc
 # =============================================================================
 # Import Cross Section Data
 # =============================================================================
-G = 12 # G must be 12, 70, or 618
+G = 12  # G must be 12, 70, or 618
 
 script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
 rel_path = "./HDPE/"
@@ -64,7 +64,7 @@ chance = 0.9
 threshold = 1e-3
 mcdc.weight_roulette(chance, threshold)
 # Setting
-mcdc.setting(N_particle=1E3)
+mcdc.setting(N_particle=1e3)
 
 # Run
 mcdc.run()
