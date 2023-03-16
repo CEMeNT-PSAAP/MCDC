5ae39236209872ce2be48ffb862f4279c382dc1e
Sam Pasmann <spasmann@nd.edu>
2023-03-15 18:42:18 -0600

    Automated iQMC Material Index

--- test/regression/eigenvalue/slab_kornreich_iqmc/test.py
+++ test/regression/eigenvalue/slab_kornreich_iqmc/test.py
@@ -43,8 +43,6 @@ def test():
     Nx = len(x) - 1
     generator = "halton"
     fixed_source = np.zeros(Nx)
-    material_idx = np.zeros(Nx, dtype=int)
-    material_idx[15:] = 1
     phi0 = np.ones((Nx))
 
     # =========================================================================
@@ -55,7 +53,6 @@ def test():
         x=x,
         fixed_source=fixed_source,
         phi0=phi0,
-        material_idx=material_idx,
         maxitt=maxit,
         tol=tol,
         generator=generator,
