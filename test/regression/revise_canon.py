e314e08c488dca69a276fb0d5a947b7bca53618f
Braxton Cuneo <braxton.s.cuneo@gmail.com>
2023-08-21 15:48:54 -0700

    Added script to quickly replace old regression answers with new ones

---
+++ test/regression/revise_canon.py
@@ -0,0 +1,14 @@
+import os
+
+
+test_dirs = [ "fixed_source", "eigenvalue" ]
+
+for dir in test_dirs:
+    for task in os.scandir("./"+dir):
+        print(task)
+        os.chdir(task)
+        os.system("mv output.h5 answer.h5")
+        os.chdir(r"../..")
+
+
+
