d28cb1b47534f2d7df8244443e8180a692cd02b9
Braxton Cuneo <braxton.s.cuneo@gmail.com>
2023-08-23 15:59:08 -0700

    Both regression and verification tests pass

--- test/verification/analytic/run.py
+++ test/verification/analytic/run.py
@@ -1,11 +1,15 @@
 import numpy as np
 import os
 import sys
+import subprocess
 
 if len(sys.argv) > 1:
     N_proc = int(sys.argv[1])
 else:
     N_proc = 1
+
+
+waitlist = []
 
 # Fixed source
 N_min = 3
@@ -16,12 +20,15 @@ for task in os.scandir("./fixed_source"):
         if not os.path.isfile("output_" + str(int(N_hist)) + ".h5"):
             print(task, int(N_hist))
             if N_proc == 1:
-                os.system("python input.py --mode=numba %i" % (N_hist))
+                waitlist.append(subprocess.Popen(("python input.py --mode=numba %i" % (N_hist)).split()))
             else:
                 os.system(
                     "srun -n %i python input.py --mode=numba %i" % (N_proc, N_hist)
                 )
     os.chdir(r"../..")
+
+for proc in waitlist:
+    proc.wait()
 
 # Eigenvalue
 """
