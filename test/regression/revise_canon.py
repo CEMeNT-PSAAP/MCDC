05f642239d067e716f1c6d273fbf76b441634596
ilhamv <variansyah_ilham@gmail.com>
2023-08-28 22:12:45 -0700

    revise regression test answers

--- test/regression/revise_canon.py
+++ test/regression/revise_canon.py
@@ -5,8 +5,6 @@ test_dirs = ["fixed_source", "eigenvalue"]
 
 for dir in test_dirs:
     for task in os.scandir("./" + dir):
-        print(task)
         os.chdir(task)
-        os.system("pytest")
         os.system("mv output.h5 answer.h5")
         os.chdir(r"../..")
