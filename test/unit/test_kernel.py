6e66203a2cc7c5078834c61d860e2a930ef6fc47
spasmann <46267220+spasmann@users.noreply.github.com>
2023-09-08 14:26:28 -0600

    reset card in kernel unit tests

--- test/unit/test_kernel.py
+++ test/unit/test_kernel.py
@@ -1,6 +1,7 @@
 import numpy as np
 import mcdc as MCDC
 from mcdc import type_
+from mcdc.main import closeout
 from mcdc.kernel import rng, AxV, FxV, HxV, preconditioner
 import mcdc.global_ as mcdc_
 
@@ -68,61 +69,37 @@ def iqmc_dummy_mcdc_variable():
     return MCDC.prepare()
 
 
-# def test_rn_basic():
-#     """
-#     Basic test routine for random number generator.
-#     Get the first 5 random numbers, skip a few, get 5 more and compare to
-#     reference data from [1]
+def test_rn_basic():
+    """
+    Basic test routine for random number generator.
+    Get the first 5 random numbers, skip a few, get 5 more and compare to
+    reference data from [1]
 
-#     Seed numbers for index 1-5, 123456-123460
+    Seed numbers for index 1-5, 123456-123460
 
-#     [1] F. B. Brown, “Random number generation with arbitrary strides”,
-#         Trans. Am. Nucl. Soc, 71, 202 (1994)
+    [1] F. B. Brown, “Random number generation with arbitrary strides”,
+        Trans. Am. Nucl. Soc, 71, 202 (1994)
 
-#     """
-#     ref_data = np.array(
-#         (
-#             1,
-#             2806196910506780710,
-#             6924308458965941631,
-#             7093833571386932060,
-#             4133560638274335821,
-#         )
-#     )
+    """
+    MCDC.reset_cards()
+    
+    ref_data = np.array(
+        (
+            1,
+            2806196910506780710,
+            6924308458965941631,
+            7093833571386932060,
+            4133560638274335821,
+        )
+    )
 
-#     # PRNG parameters from [1]
-#     g = 2806196910506780709
-#     c = 1
-#     seed0 = 1
-#     mod = 2**63
+    mcdc = iqmc_dummy_mcdc_variable()
 
-#     # Arbitrary constants for dummy mcdc container
-#     G = 1
-#     J = 1
-#     Nmax_slice = 1
-#     Nmax_surface = 1
-#     Nmax_cell = 1
-#     # need a psuedo material for mcdc container
-#     input_deck.materials.append({"G": 1, "J": 1})
-
-#     # Make types for dummy mcdc container
-#     type_.make_type_material(G, J, 1)
-#     type_.make_type_surface(Nmax_slice)
-#     type_.make_type_cell(Nmax_surface)
-#     type_.make_type_universe(Nmax_cell)
-#     type_.make_type_lattice(input_deck.lattices)
-#     type_.make_type_source(G)
-#     type_.make_type_tally(1, input_deck.tally)
-#     type_.make_type_technique(0, 1, input_deck.technique)
-#     type_.make_type_global(input_deck)
-
-#     # The dummy container
-#     mcdc = np.zeros(1, dtype=type_.global_)[0]
-
-#     # run through the first five seeds (1-5)
-#     for i in range(5):
-#         rng(mcdc["setting"])
-#         assert mcdc["setting"]["rng_seed"] == ref_data[i]
+    # run through the first five seeds (1-5)
+    for i in range(5):
+        assert mcdc["setting"]["rng_seed"] == ref_data[i]
+        rng(mcdc["setting"])
+    
 
 
 def test_AxV_linearity():
@@ -137,6 +114,8 @@ def test_AxV_linearity():
     We can test both properties with:
             - f(a*x + b*y) = a*f(x) + b*f(y)
     """
+    MCDC.reset_cards()
+    
     mcdc = iqmc_dummy_mcdc_variable()
     
     size = mcdc["technique"]["iqmc_flux"].size
@@ -164,6 +143,8 @@ def test_FxV_linearity():
     We can test both properties with:
             - f(a*x + b*y) = a*f(x) + b*f(y)
     """
+    MCDC.reset_cards()
+    
     mcdc = iqmc_dummy_mcdc_variable()
     
     size = mcdc["technique"]["iqmc_flux"].size
@@ -190,6 +171,8 @@ def test_HxV_linearity():
     We can test both properties with:
             - f(a*x + b*y) = a*f(x) + b*f(y)
     """
+    MCDC.reset_cards()
+    
     mcdc = iqmc_dummy_mcdc_variable()
     
     size = mcdc["technique"]["iqmc_flux"].size
@@ -216,6 +199,8 @@ def test_preconditioner_linearity():
     We can test both properties with:
             - f(a*x + b*y) = a*f(x) + b*f(y)
     """
+    MCDC.reset_cards()
+    
     mcdc = iqmc_dummy_mcdc_variable()
     
     size = mcdc["technique"]["iqmc_flux"].size
@@ -231,8 +216,9 @@ def test_preconditioner_linearity():
     
     
 
-if __name__ == "__main__":
-    test_AxV_linearity()
-    test_FxV_linearity()
-    test_HxV_linearity()
-    test_preconditioner_linearity()
+# if __name__ == "__main__":
+    # test_rn_basic()
+    # test_AxV_linearity()
+#     test_FxV_linearity()
+#     test_HxV_linearity()
+#     test_preconditioner_linearity()
