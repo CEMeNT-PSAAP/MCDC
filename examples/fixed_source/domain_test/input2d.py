ea11c02c1e495d41390ef9527c5f76394a5c5926
ilhamv <variansyah_ilham@gmail.com>
2023-09-27 20:27:44 -0700

    use print_error and back in black

--- examples/fixed_source/domain_test/input2d.py
+++ examples/fixed_source/domain_test/input2d.py
@@ -3,7 +3,7 @@ import numpy as np
 import mcdc
 
 m1 = mcdc.material(capture=np.array([0.0]))
-m2 = mcdc.material(capture=np.array([3.5]) )
+m2 = mcdc.material(capture=np.array([3.5]))
 
 s1 = mcdc.surface("plane-x", x=0.0, bc="vacuum")
 s2 = mcdc.surface("plane-x", x=3.0, bc="vacuum")
@@ -12,15 +12,14 @@ s3 = mcdc.surface("plane-z", z=0.0, bc="vacuum")
 s4 = mcdc.surface("plane-z", z=3.0, bc="vacuum")
 
 
-
-mcdc.cell([+s1,-s2,+s3,-s4],m1)
-mcdc.source(x=[0.0, 3.0],z=[0.0,1e-10],isotropic=True, prob=1)  
-mcdc.source(x=[0.0, 1e-10],z=[0.0,3.0],isotropic=True, prob=1) 
+mcdc.cell([+s1, -s2, +s3, -s4], m1)
+mcdc.source(x=[0.0, 3.0], z=[0.0, 1e-10], isotropic=True, prob=1)
+mcdc.source(x=[0.0, 1e-10], z=[0.0, 3.0], isotropic=True, prob=1)
 
 
 scores = ["flux", "current"]
 mcdc.tally(scores=scores, x=np.linspace(0.0, 3.0, 4), z=np.linspace(0.0, 3.0, 4))
 
 mcdc.setting(N_particle=1e5)
-mcdc.domain_decomp(z=np.linspace(0.0, 3.0, 4),x=np.linspace(0.0,3.0,4)) 
+mcdc.domain_decomp(z=np.linspace(0.0, 3.0, 4), x=np.linspace(0.0, 3.0, 4))
 mcdc.run()
