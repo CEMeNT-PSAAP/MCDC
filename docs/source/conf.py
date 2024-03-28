4a8f083b0132e7d0af5dab272a07aec8b6b03d44
Kayla Clements <clemekay@oregonstate.edu>
2024-03-28 13:33:00 -0700

    Add colorama mock for Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 # On Read the Docs, need to mock any python packages that would require c
 from unittest.mock import MagicMock
 
-MOCK_MODULES = ["mpi4py"]
+MOCK_MODULES = ["mpi4py", "colorama"]
 sys.modules.update((mod_name, MagicMock()) for mod_name in MOCK_MODULES)
 
 
