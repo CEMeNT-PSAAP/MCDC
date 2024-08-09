f7c68414d4847e0818578b5ce9d148f10dc206a7
Sam Pasmann <spasmann@nd.edu>
2024-08-09 12:43:37 -0400

    input validation function and updated example/test input decks

--- test/regression/iqmc_cooper2/input.py
+++ test/regression/iqmc_cooper2/input.py
@@ -53,7 +53,7 @@ mcdc.iQMC(
     phi0=phi0,
     maxit=maxit,
     tol=tol,
-    score=["source-x", "source-y"],
+    scores=["source-x", "source-y"],
 )
 
 # =============================================================================
