c70c07c45994535ba699e78bd35a522973d4db9e
Ilham Variansyah <variansi@oregonstate.edu>
2024-08-15 20:41:08 +0700

    back in black

--- mcdc/local.py
+++ mcdc/local.py
@@ -4,6 +4,7 @@ Collection of functions to make local array and object
 All of the functions are target-dependent (CPU or GPU mode)
 and defined in code_factory.py
 """
+
 translation = None
 energy_group_array = None
 precursor_group_array = None
