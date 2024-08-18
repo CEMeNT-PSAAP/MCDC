d51e1163a9fa7db75a0e58c920586b7d7de7d428
Ilham Variansyah <variansi@oregonstate.edu>
2024-08-18 14:22:08 +0700

    forgot to import in iqmc_loop

--- mcdc/iqmc/iqmc_loop.py
+++ mcdc/iqmc/iqmc_loop.py
@@ -4,6 +4,7 @@ from numpy import ascontiguousarray as cga
 from numba import njit, objmode
 
 import mcdc.adapt as adapt
+import mcdc.geometry as geometry
 import mcdc.iqmc.iqmc_kernel as iqmc_kernel
 import mcdc.kernel as kernel
 import mcdc.local as local
