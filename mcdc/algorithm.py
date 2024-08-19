219558e770cea302b9a603c75a584d0d95375c69
Ilham Variansyah <variansi@oregonstate.edu>
2024-08-19 14:02:43 +0700

    revert binary_search for GPU-compatibility

--- mcdc/algorithm.py
+++ mcdc/algorithm.py
@@ -2,10 +2,10 @@ from numba import njit
 
 
 @njit
-def binary_search(val, grid, length=0):
+def binary_search_with_length(val, grid, length):
     """
-    Binary search that returns the bin index of the value `val` given grid `grid`.
-    Only search up to `length`-th element if `length` is given.
+    Binary search that returns the bin index of the value `val` given grid `grid`
+    Only search up to `length`-th element
 
     Some special cases:
         val < min(grid)  --> -1
@@ -26,3 +26,12 @@ def binary_search(val, grid, length=0):
         else:
             right = mid - 1
     return int(right)
+
+
+@njit
+def binary_search(val, grid):
+    """
+    Binary search with full length of the given grid.
+    See binary_search_with _length
+    """
+    return binary_search_with_length(val, grid, 0)
