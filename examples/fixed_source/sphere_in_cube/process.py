86870a6f1e209249152307015de1ab941d87ec75
Ethan Lame <ethan0824@gmail.com>
2024-10-03 11:23:18 -0700

    Sphere in cube, made process.py faster by removing unnecessary module imports

--- examples/fixed_source/sphere_in_cube/process.py
+++ examples/fixed_source/sphere_in_cube/process.py
@@ -1,15 +1,7 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import h5py
-import matplotlib.colors as colors
 
 # Load results
 with h5py.File("output.h5", "r") as f:
-    print("Keys: %s" % f.keys())
-    tallies = f["tallies"]
-    print("Tallies keys: %s" %tallies.keys())
-    print("Cell keys: %s" %tallies["cell_tally_0"].keys())
-    print("Fission keys: %s" %tallies["cell_tally_0"]["fission"].keys())
-
-    cell = f["tallies/cell_tally_0/fission/mean"][()]
-    print(f'cell tally = {cell}')
+    cell = f["tallies/cell_tally_0/fission"]
+    print(f'sphere mean = {cell["mean"][()]}')    
+    print(f'sphere sdev = {cell["sdev"][()]}')
