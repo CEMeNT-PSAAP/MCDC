336c25e873613b460bb50924a133100465d001cf
Ilham Variansyah <variansi@oregonstate.edu>
2024-11-11 08:40:39 +0700

    add a coincidence tolerance for time crossings

--- mcdc/src/mesh/structured.py
+++ mcdc/src/mesh/structured.py
@@ -1,7 +1,7 @@
 from numba import njit
 
 from mcdc.src.algorithm import binary_search
-from mcdc.constant import COINCIDENCE_TOLERANCE, INF
+from mcdc.constant import COINCIDENCE_TOLERANCE, COINCIDENCE_TOLERANCE_TIME, INF
 
 
 @njit
@@ -30,8 +30,8 @@ def get_indices(particle_container, mesh):
         or y > mesh["y"][-1] + COINCIDENCE_TOLERANCE
         or z < mesh["z"][0] - COINCIDENCE_TOLERANCE
         or z > mesh["z"][-1] + COINCIDENCE_TOLERANCE
-        or t < mesh["t"][0] - COINCIDENCE_TOLERANCE
-        or t > mesh["t"][-1] + COINCIDENCE_TOLERANCE
+        or t < mesh["t"][0] - COINCIDENCE_TOLERANCE_TIME
+        or t > mesh["t"][-1] + COINCIDENCE_TOLERANCE_TIME
         # At the outermost-grid but moving away
         or (abs(x - mesh["x"][0]) < COINCIDENCE_TOLERANCE and ux < 0.0)
         or (abs(x - mesh["x"][-1]) < COINCIDENCE_TOLERANCE and ux > 0.0)
@@ -39,7 +39,7 @@ def get_indices(particle_container, mesh):
         or (abs(y - mesh["y"][-1]) < COINCIDENCE_TOLERANCE and uy > 0.0)
         or (abs(z - mesh["z"][0]) < COINCIDENCE_TOLERANCE and uz < 0.0)
         or (abs(z - mesh["z"][-1]) < COINCIDENCE_TOLERANCE and uz > 0.0)
-        or (abs(t - mesh["t"][-1]) < COINCIDENCE_TOLERANCE)
+        or (abs(t - mesh["t"][-1]) < COINCIDENCE_TOLERANCE_TIME)
     ):
         outside = True
         return -1, -1, -1, -1, outside
@@ -47,7 +47,9 @@ def get_indices(particle_container, mesh):
     ix = _grid_index(x, ux, mesh["x"])
     iy = _grid_index(y, uy, mesh["y"])
     iz = _grid_index(z, uz, mesh["z"])
-    it = _grid_index(t, 1.0, mesh["t"])  # Particle always moves forward in time
+    it = _grid_index(
+        t, 1.0, mesh["t"], COINCIDENCE_TOLERANCE_TIME
+    )  # Particle always moves forward in time
 
     return ix, iy, iz, it, outside
 
@@ -72,7 +74,7 @@ def get_crossing_distance(particle_arr, speed, mesh):
     # Check if particle is outside the mesh grid and moving away
     outside = False
     if (
-        (t > mesh["t"][-1] - COINCIDENCE_TOLERANCE)
+        (t > mesh["t"][-1] - COINCIDENCE_TOLERANCE_TIME)
         or (x < mesh["x"][0] + COINCIDENCE_TOLERANCE and ux < 0.0)
         or (x > mesh["x"][-1] - COINCIDENCE_TOLERANCE and ux > 0.0)
         or (y < mesh["y"][0] + COINCIDENCE_TOLERANCE and uy < 0.0)
@@ -86,12 +88,12 @@ def get_crossing_distance(particle_arr, speed, mesh):
     d = min(d, _grid_distance(x, ux, mesh["x"]))
     d = min(d, _grid_distance(y, uy, mesh["y"]))
     d = min(d, _grid_distance(z, uz, mesh["z"]))
-    d = min(d, _grid_distance(t, 1.0 / speed, mesh["t"]))
+    d = min(d, _grid_distance(t, 1.0 / speed, mesh["t"], COINCIDENCE_TOLERANCE_TIME))
     return d
 
 
 @njit
-def _grid_index(value, direction, grid):
+def _grid_index(value, direction, grid, tolerance=COINCIDENCE_TOLERANCE):
     """
     Get grid index given the value and the direction
 
@@ -103,17 +105,17 @@ def _grid_index(value, direction, grid):
 
     # Coinciding cases
     if direction > 0.0:
-        if abs(grid[idx + 1] - value) < COINCIDENCE_TOLERANCE:
+        if abs(grid[idx + 1] - value) < tolerance:
             idx += 1
     else:
-        if abs(grid[idx] - value) < COINCIDENCE_TOLERANCE:
+        if abs(grid[idx] - value) < tolerance:
             idx -= 1
 
     return idx
 
 
 @njit
-def _grid_distance(value, direction, grid):
+def _grid_distance(value, direction, grid, tolerance=COINCIDENCE_TOLERANCE):
     """
     Get distance to nearest grid given a value and direction
 
@@ -130,7 +132,7 @@ def _grid_distance(value, direction, grid):
         idx += 1
 
     # Coinciding cases
-    if abs(grid[idx] - value) < COINCIDENCE_TOLERANCE:
+    if abs(grid[idx] - value) < tolerance:
         if direction > 0.0:
             idx += 1
         else:
