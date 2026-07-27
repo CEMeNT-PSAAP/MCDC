d3f32c3ce74ca0996cd8f5565ced0142e2fe9c33
Ilham Variansyah <variansi@oregonstate.edu>
2026-07-28 06:08:18 +0700

    Update examples

--- examples/kobayashi-TD/input.py
+++ examples/kobayashi-TD/input.py
@@ -1,6 +1,8 @@
 import numpy as np
 import mcdc
 
+simulation = mcdc.Simulation("Time-dependent Kobayashi dog-leg benchmark")
+
 # ======================================================================================
 # Set model
 # ======================================================================================
@@ -30,24 +32,25 @@ sz5 = mcdc.Surface.PlaneZ(z=60.0, boundary_condition="vacuum")
 
 # Set cells
 # Source
-mcdc.Cell(region=+sx1 & -sx2 & +sy1 & -sy2 & +sz1 & -sz2, fill=m)
+source_cell = mcdc.Cell(region=+sx1 & -sx2 & +sy1 & -sy2 & +sz1 & -sz2, fill=m)
 # Voids
 channel_1 = +sx1 & -sx2 & +sy2 & -sy3 & +sz1 & -sz2
 channel_2 = +sx1 & -sx3 & +sy3 & -sy4 & +sz1 & -sz2
 channel_3 = +sx3 & -sx4 & +sy3 & -sy4 & +sz1 & -sz3
 channel_4 = +sx3 & -sx4 & +sy3 & -sy5 & +sz3 & -sz4
 void_channel = channel_1 | channel_2 | channel_3 | channel_4
-mcdc.Cell(region=void_channel, fill=m_void)
+void_cell = mcdc.Cell(region=void_channel, fill=m_void)
 # Shield
 box = +sx1 & -sx5 & +sy1 & -sy5 & +sz1 & -sz5
-mcdc.Cell(region=box & ~void_channel, fill=m)
+shield_cell = mcdc.Cell(region=box & ~void_channel, fill=m)
+simulation.set_model([source_cell, void_cell, shield_cell])
 
 # ======================================================================================
 # Set source
 # ======================================================================================
-# The source pulses in t=[0,5]
+# The source pulses in t=[0, 50]
 
-mcdc.Source(
+source = mcdc.Source(
     x=[0.0, 10.0],
     y=[0.0, 10.0],
     z=[0.0, 10.0],
@@ -55,6 +58,7 @@ mcdc.Source(
     energy_group=0,
     time=[0.0, 50.0],
 )
+simulation.set_sources([source])
 
 # ======================================================================================
 # Set tallies, settings, techniques, and run MC/DC
@@ -63,15 +67,16 @@ mcdc.Source(
 # Tallies
 time_grid = np.linspace(0.0, 200.0, 21)
 mesh = mcdc.MeshUniform(x=(0.0, 1.0, 60), y=(0.0, 1.0, 100))
-mcdc.Tally(mesh=mesh, scores=["flux"], time=time_grid)
-mcdc.Tally(scores=["density"], time=time_grid)
+flux_tally = mcdc.Tally(mesh=mesh, scores=["flux"], time=time_grid)
+density_tally = mcdc.Tally(scores=["density"], time=time_grid)
+simulation.set_tallies([flux_tally, density_tally])
 
 # Settings
-mcdc.settings.N_particle = 100
-mcdc.settings.N_batch = 2
+simulation.settings.N_particle = 100
+simulation.settings.N_batch = 2
 
 # Techniques
-mcdc.simulation.implicit_capture()
+simulation.implicit_capture()
 
 # Run
-mcdc.run()
+simulation.run()
